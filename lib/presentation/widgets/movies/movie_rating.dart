0da2bb934f093d5f32726b18f7af87f9af919f99
Ernesto Alfonso Sanchez Montiel <ernest.alf45@gmail.com>
2023-06-25 12:29:59 -0600

    Puntos de interrupción

--- lib/presentation/widgets/movies/movie_rating.dart
+++ lib/presentation/widgets/movies/movie_rating.dart
@@ -13,6 +13,7 @@ class MovieRating extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
 
 
     return SizedBox(
@@ -24,7 +25,7 @@ class MovieRating extends StatelessWidget {
           const SizedBox(width: 8),
           const SizedBox(width: 3),
           Text(HumanFormats.number(voteAverage, 1),
-              style: const TextStyle(fontSize: 17)
+              style: textStyles.bodyMedium
                   ?.copyWith(color: Colors.yellow.shade800)),
           
         ],
